ee0d20add857176e0c6730939d5a5210910a0f02
jgeddes3 <59842820+jgeddes3@users.noreply.github.com>
2023-06-13 23:45:37 -0500

    instagram bot initial test

---
+++ instagramUnfollow.py
@@ -0,0 +1,23 @@
+import instaloader
+
+# Instantiate
+L = instaloader.Instaloader()
+
+# Login or load session
+username = '' # your username
+password = '' # your password
+L.login(username, password) # (login)
+
+# Obtain profile metadata
+profile = instaloader.Profile.from_username(L.context, username)
+
+# List of usernames that YOU are following
+follow_list = [followee.username for followee in profile.get_followees()]
+
+# List of usernames that are following YOU
+followers_list = [follower.username for follower in profile.get_followers()]
+
+# Finding the difference between the lists (People you follow, but who don't follow back)
+non_followers = [user for user in follow_list if user not in followers_list]
+
+print('People who do not follow you back:', ', '.join(non_followers))
